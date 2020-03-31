cabb98414de4e15d0bf0e3edc89d5a010a4c385a
Frogi <sebastian.przyszlak@gmail.com>
2020-03-31 15:16:59 +0200

    31.03.2020

---
+++ []Killer/Assets/Scripts/CandleFlameLight.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CandleFlameLight : MonoBehaviour
+{
+
+    public int minIntensity = 30;
+    public int maxIntensity = 40;
+
+    private int minRange = 22;
+    private int maxRange = 25;
+
+    private int frames = 0;
+
+    public Light candleFlameLight;
+    // Update is called once per frame
+    void Update()
+    {
+        if(++frames > 10)
+        {
+            candleFlameLight.intensity = Random.Range(minIntensity, maxIntensity);
+            candleFlameLight.range = Random.Range(minRange, maxRange);
+            
+            frames = 0;
+        }
+    }
+}
