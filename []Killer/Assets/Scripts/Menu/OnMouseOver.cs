618d1b8e9d458d904887760ecadf9b8e34d4c351
Frogi <sebastian.przyszlak@gmail.com>
2020-04-06 00:15:22 +0200

    OnMouseOverSoundTest

---
+++ []Killer/Assets/Scripts/Menu/OnMouseOver.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine.EventSystems;
+using UnityEngine;
+
+public class OnMouseOver : MonoBehaviour, IPointerEnterHandler
+{
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        FindObjectOfType<AudioManager>().Play("ButtonHoverSound");
+    }
+}
