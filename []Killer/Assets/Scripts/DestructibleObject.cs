ad967a5cb781d09e853fd52a0f9977f7f00f2c29
Frogi <sebastian.przyszlak@gmail.com>
2020-04-27 07:57:42 +0200

    Zniszczalne przedmioty - rigidbody
    dodanie rigidbody do zniszczalnych przedmiotów,
    zwiększenie ImpactForce dla shotguna
    implementacja interfejscu IHavePixels w zniszczalnych przedmiotach

--- []Killer/Assets/Scripts/DestructibleObject.cs
+++ []Killer/Assets/Scripts/DestructibleObject.cs
@@ -1,13 +1,21 @@
 ﻿using UnityEngine;
 using UnityEngine.VFX;
 
-public class DestructibleObject : MonoBehaviour, ITakeDamage<int>
+public class DestructibleObject : MonoBehaviour, ITakeDamage<int>, IHavePiksels
 {
-    private int maxHealth = 15;
+    [Header("Health")]
+    [SerializeField] private int maxHealth = 15;
     private int health = 15;
     public int MaxHealth { get { return maxHealth; } }
     public int Health { get { return health; } }
 
+    [Header("PixelDrop")]
+    [SerializeField] private int minPixelsDrop = 5;
+    [SerializeField] private int maxPixelsDrop = 15;
+    public int MinPixelsDrop { get { return minPixelsDrop; } }
+    public int MaxPixelsDrop { get { return maxPixelsDrop; } }
+
+    [Header("Destroy VFX")]
     public GameObject destroyEffectPrefab;
     private SoundManager soundManager;
 
@@ -19,14 +27,15 @@ public class DestructibleObject : MonoBehaviour, ITakeDamage<int>
             
     public void Die()
     {
+        int pixels = DropPixels();
+
+        Debug.Log("I drop " + pixels + " pixels!");
+
         GameObject destroyEffectGO = Instantiate(destroyEffectPrefab, transform.position, Quaternion.identity);
         VisualEffect destroyEffect = destroyEffectGO.GetComponent<VisualEffect>();
 
-        Debug.Log("Obcject Die");
-
         if(destroyEffect != null)
         {
-            Debug.Log("Obcject destroyEffect");
             MeshRenderer myRenderer = gameObject.GetComponent<MeshRenderer>();
             if(myRenderer != null)
             {
@@ -48,5 +57,12 @@ public class DestructibleObject : MonoBehaviour, ITakeDamage<int>
 
         if (health <= 0)
             Die();
+    }
+
+    public int DropPixels()
+    {
+        int Pixels = Random.Range(MinPixelsDrop, MaxPixelsDrop);
+        PlayerManager.instance.player.GetComponent<Player>().Pixels += Pixels;
+        return Pixels;
     }
 }
