fc84fe9eefce312909f647e962e04a9b8125572b
anmach <ania.m10110@gmail.com>
2020-05-04 22:37:36 +0200

    Spawner dla prefabów

---
+++ []Killer/Assets/Scripts/Spawner.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public Transform spawnedContainer;
+    public GameObject prefabToSpawn;
+
+    public float timeBetweenSpawns = 10;
+    public int maxObjectsSpawned = 10;
+
+    private float timer = 0;
+
+    private void Start()
+    {
+        if(spawnedContainer == null)
+        {
+            spawnedContainer = gameObject.transform.parent;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if(timer >= timeBetweenSpawns)
+        {
+            timer = 0;
+            if(spawnedContainer.childCount < maxObjectsSpawned)
+            {
+                Instantiate(prefabToSpawn, transform.position, Quaternion.identity, spawnedContainer);
+            }
+        }
+    }
+}
