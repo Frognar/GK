9f2b95fa6bda05a322892f606680d6d4b13770d9
anmach <ania.m10110@gmail.com>
2020-05-12 16:34:30 +0200

    Teleport to new world

---
+++ []Killer/Assets/Scripts/HelpDontKnowWhereToPutIt/Teleporter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Anna Mach 12.05.2020
+public class Teleporter : MonoBehaviour
+{
+    public GameObject placeToTeleport;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.gameObject.transform.position = placeToTeleport.transform.position;
+        }
+    }
+}
